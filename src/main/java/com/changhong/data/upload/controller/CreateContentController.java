5a25780d4fe881f5b5921b19d3ce97d8fd1f7a50
BBSee <1017498887@qq.com>
2017-03-31 14:52:02 +0800

    midify name

--- src/main/java/com/changhong/data/upload/controller/CreateContentController.java
+++ src/main/java/com/changhong/data/upload/controller/CreateContentController.java
@@ -1,4 +1,4 @@
-package com.changhong.data.upload.CONTROLLER;
+package com.changhong.data.upload.controller;
 
 import org.springframework.stereotype.Controller;
 
--- src/main/java/com/changhong/data/upload/controller/UploadController.java
+++ src/main/java/com/changhong/data/upload/controller/UploadController.java
@@ -1,4 +1,4 @@
-package com.changhong.data.upload.CONTROLLER;
+package com.changhong.data.upload.controller;
 
 import com.changhong.data.upload.entity.SingleuploadResponse;
 import com.changhong.data.upload.service.QueryStringAnalysisService;
